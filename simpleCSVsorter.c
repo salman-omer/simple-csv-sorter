5e738afb4334871cb938bb14bdecea286aba07b9
Shane Park <seyeon9897@gmail.com>
2018-09-20 22:01:23 -0400

    minor update

--- simpleCSVsorter.c
+++ simpleCSVsorter.c
@@ -5,12 +5,15 @@
 #include "simpleCSVsorter.h"
 
 int
-main(int argc, char* argv[]){
+main(int argc, char *argv[]){
     //check if there are correct # of argument inputs
     if (argc == 3){
         //check if command is correct
         if (strcmp(argv[1], "-c") == 0){
-//            if (){
+            //check if column name is valid
+//            if (){        - loop here to go thru and check all types
+//
+//            }else{
 //
 //            }
             printf("SUCCESS!\n\n");
@@ -18,6 +21,7 @@ main(int argc, char* argv[]){
             printf("Usage: simpleCSVsorter -c <column name>\n\n");
         }
     }else {
+//        fprintf(stderr, "\nERROR: ENTER IN CORRECT NUMBER OF ARGUMENTS\n");
         printf("\nERROR: ENTER IN CORRECT NUMBER OF ARGUMENTS\n");
     }
     return 0;
