dcea55aa4e46d20addb35d9a5b8a45c458d61d6e
Shane Park <seyeon9897@gmail.com>
2018-09-23 14:44:05 -0400

    mergeSort WIP

---
+++ mergesort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//For sorting numeric types
+//
+void numMerge(int arr[])
+{
+
+}
+
+//l = leftIndex, r = rightIndex
+//recursive call
+void numMergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (l + r)/2;
+
+
+    }
+}
+
+//For soring String types
+void strMerge(char *arr[])
+{
+
+}
+
+void strMergeSort(char *arr[])
+{
+
+}
