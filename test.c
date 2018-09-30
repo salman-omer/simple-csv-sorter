3b33ddbc9c9a0ca782effc059ea10ec93f0d4aa5
Salman K. Omer <sko25@singleton.cs.rutgers.edu>
2018-09-30 00:19:39 -0400

    Read in csv and store in movielines struct linked list

--- test.c
+++ test.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <math.h>
 
+typedef enum { false, true } bool;
+
 const int DEBUG = 2;
 const int DEBUG2 = 0;
 
@@ -48,132 +50,393 @@ typedef struct _moveLineLL
 int initMovieLine(movieLine* movie){
 	movie-> color = NULL;
 	movie-> director_name = NULL;
-	movie-> num_critic_for_reviews = -1;
-	movie-> duration = -1;
-	movie-> director_facebook_likes = -1;
-	movie-> actor_3_facebook_likes = -1;
+	movie-> num_critic_for_reviews = 0;
+	movie-> duration = 0;
+	movie-> director_facebook_likes = 0;
+	movie-> actor_3_facebook_likes = 0;
 	movie-> actor_2_name = NULL;
-	movie-> actor_1_facebook_likes = -1;
-	movie-> gross = -1;
+	movie-> actor_1_facebook_likes = 0;
+	movie-> gross = 0;
 	movie-> genres = NULL;
 	movie-> actor_1_name = NULL;
 	movie-> movie_title = NULL;
-	movie-> num_voted_users = -1;
-	movie-> cast_total_facebook_likes = -1;
+	movie-> num_voted_users = 0;
+	movie-> cast_total_facebook_likes = 0;
 	movie-> actor_3_name = NULL;
-	movie-> facenumber_in_poster = -1;
+	movie-> facenumber_in_poster = 0;
 	movie-> plot_keywords = NULL;
 	movie-> movie_imdb_link = NULL;
-	movie-> num_user_for_reviews = -1;
+	movie-> num_user_for_reviews = 0;
 	movie-> language = NULL;
 	movie-> country = NULL;
 	movie-> content_rating = NULL;
-	movie-> budget = -1;
-	movie-> title_year = -1;
-	movie-> actor_2_facebook_likes = -1;
-	movie-> imdb_score = -1;
-	movie-> aspect_ratio = -1;
-	movie-> movie_facebook_likes = -1;
+	movie-> budget = 0;
+	movie-> title_year = 0;
+	movie-> actor_2_facebook_likes = 0;
+	movie-> imdb_score = 0;
+	movie-> aspect_ratio = 0;
+	movie-> movie_facebook_likes = 0;
+	movie->next = NULL;
+	return 0;
 }
+
+/*
+char* getColumnValue(movieLine* movie, char* columnName){
+	if (strcmp(columnName, "color")){
+		return movie->color;
+	}
+	else if (strcmp(columnName, "director_name")){
+		return movie->director_name;
+	}
+	else if (strcmp(columnName, "num_critic_for_reviews")){
+		int num = movie->num_critic_for_reviews;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "duration")){
+		int num = movie->duration;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "director_facebook_likes")){
+		int num = movie->director_facebook_likes;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;	
+	}
+	else if (strcmp(columnName, "actor_3_facebook_likes")){
+		int num = movie->actor_3_facebook_likes;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "actor_2_name")){
+		return movie->actor_2_name;
+	}
+	else if (strcmp(columnName, "actor_1_facebook_likes")){
+		int num = movie->actor_1_facebook_likes;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "gross")){
+		int num = movie->gross;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;	
+	}
+	else if (strcmp(columnName, "genres")){
+		return movie->genres;
+	}
+	else if (strcmp(columnName, "actor_1_name")){
+		return movie->actor_1_name;
+	}
+	else if (strcmp(columnName, "movie_title")){
+		return movie->movie_title;
+	}
+	else if (strcmp(columnName, "num_voted_users")){
+		int num = movie->num_voted_users;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "cast_total_facebook_likes")){
+		int num = movie->cast_total_facebook_likes;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "actor_3_name")){
+		 return movie->actor_3_name;
+	}
+	else if (strcmp(columnName, "facenumber_in_poster")){
+		int num = movie->facenumber_in_poster;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "plot_keywords")){
+		return movie->plot_keywords;
+	}
+	else if (strcmp(columnName, "movie_imdb_link")){
+		return movie->movie_imdb_link;
+	}
+	else if (strcmp(columnName, "num_user_for_reviews")){
+		int num = movie->num_user_for_reviews;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "language")){
+		return movie->language;
+	}
+	else if (strcmp(columnName, "country")){
+		return movie->country;
+	}
+	else if (strcmp(columnName, "content_rating")){
+		return movie->content_rating;
+	}
+	else if (strcmp(columnName, "budget")){
+		int num = movie->budget;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "title_year")){
+		int num = movie->title_year;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "actor_2_facebook_likes")){
+		int num = movie->actor_2_facebook_likes;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	else if (strcmp(columnName, "imdb_score")){
+		int num = movie->imdb_score;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%f", num);
+		return value;
+	}
+	else if (strcmp(columnName, "aspect_ratio")){
+		int num = movie->aspect_ratio;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%f", num);
+		return value;
+	}
+	else if (strcmp(columnName, "movie_facebook_likes")){
+		int num = movie->movie_facebook_likes;
+		char* value = malloc(20*sizeof(char));
+		sprintf(value, "%d", num);
+		return value;
+	}
+	return "Big problem here";
+}
+
+int* printMovieLineAsCsv(movieLine* movie, int numColumns, char** columnNames){
+	char line[500];
+	int lineIndex = 0;
+	int i;
+	int j;
+	for (i = 0; i < numColumns; i++)
+	{
+		char* value = getColumnValue(movie, columnNames[i]);
+		if(strcmp(columnNames[i], "num_critic_for_reviews") ||
+			strcmp(columnNames[i], "director_facebook_likes") ||
+			strcmp(columnNames[i], "actor_3_facebook_likes") ||
+			strcmp(columnNames[i], "actor_2_facebook_likes") ||
+			strcmp(columnNames[i], "actor_1_facebook_likes") ||
+			strcmp(columnNames[i], "gross") ||
+			strcmp(columnNames[i], "num_voted_users") ||
+			strcmp(columnNames[i], "cast_total_facebook_likes") ||
+			strcmp(columnNames[i], "facenumber_in_poster") ||
+			strcmp(columnNames[i], "num_user_for_reviews") ||
+			strcmp(columnNames[i], "budget") ||
+			strcmp(columnNames[i], "title_year") ||
+			strcmp(columnNames[i], "movie_facebook_likes")){
+			if(atoi(value) == 0){
+				if(i != numColumns - 1){
+					line[lineIndex] = ',';
+				} else {
+					line[lineIndex] = '\0';
+				}
+				lineIndex++;
+			} else {
+				for(j = 0; j < strlen(value); j++){
+					line[lineIndex] = value[j];
+					lineIndex++;
+				}
+				if(i == numColumns - 1){
+					line[lineIndex] = '\0';
+					continue;
+				}
+				line[lineIndex] = ',';
+				lineIndex ++;
+			}
+			continue;
+		}
+		if(strcmp(columnNames[i], "imdb_score") ||
+			strcmp(columnNames[i], "aspect_ratio")){
+			if (atof(value) == 0){
+				if(i != numColumns - 1){
+					line[lineIndex] = ',';
+				} else {
+					line[lineIndex] = '\0';
+				}
+				lineIndex++;
+			} else {
+				for(j = 0; j < strlen(value); j++){
+					line[lineIndex] = value[j];
+					lineIndex++;
+				}
+				if(i == numColumns - 1){
+					line[lineIndex] = '\0';
+					continue;
+				}
+				line[lineIndex] = ',';
+				lineIndex ++;
+			}
+			continue;
+		}
+		for (j = 0; j < strlen(value); ++j)
+		{
+			line[lineIndex] = value[j];
+			lineIndex++;
+		}
+		if(i != numColumns - 1){
+			line[lineIndex] = ',';
+		}
+	}
+
+	if (line[lineIndex] == ',')
+	{
+		line[lineIndex] = '\0';
+	}
+
+	printf("%s\n", line);
+	return 0;
+}
+
+int printMoviesAsCsv(movieLineLL* LL, int numColumns, char** columnNames){
+	movieLine* curr = LL->head;
+	while(curr != NULL){
+		printMovieLineAsCsv(curr, numColumns, columnNames);
+		curr = curr->next;
+	}
+	return 0;
+} */
 
 int addFieldToMovie(int columnNumber, char** columnNames, movieLine* movie, char* value){
 	char* columnName = columnNames[columnNumber];
 	if(DEBUG){ printf("value %s\n", value);}
-	if (strcmp(columnName, "color")){
+	if (strcmp(columnName, "color") == 0){
 		movie->color = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->color, value);
+		if (DEBUG) { printf("%s %d %s\n", "color", columnNumber, value); }
 	}
-	else if (strcmp(columnName, "director_name")){
+	else if (strcmp(columnName, "director_name") == 0){
 		movie->director_name = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->director_name, value);
+		if (DEBUG) { printf("%s %s\n", "director_name", value); }
 	}
-	else if (strcmp(columnName, "num_critic_for_reviews")){
+	else if (strcmp(columnName, "num_critic_for_reviews") == 0){
 		movie->num_critic_for_reviews = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "num_critic_for_reviews", atoi(value)); }
 	}
-	else if (strcmp(columnName, "duration")){
+	else if (strcmp(columnName, "duration") == 0){
 		movie->duration = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "duration", atoi(value)); }
 	}
-	else if (strcmp(columnName, "director_facebook_likes")){
+	else if (strcmp(columnName, "director_facebook_likes") == 0){
 		movie->director_facebook_likes = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "director_facebook_likes", atoi(value)); }
 	}
-	else if (strcmp(columnName, "actor_3_facebook_likes")){
+	else if (strcmp(columnName, "actor_3_facebook_likes") == 0){
 		movie->actor_3_facebook_likes = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "actor_3_facebook_likes", atoi(value)); }
 	}
-	else if (strcmp(columnName, "actor_2_name")){
+	else if (strcmp(columnName, "actor_2_name") == 0){
 		movie->actor_2_name = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->actor_2_name, value);
+		if (DEBUG) { printf("%s %s\n", "actor_2_name", value); }
 	}
-	else if (strcmp(columnName, "actor_1_facebook_likes")){
+	else if (strcmp(columnName, "actor_1_facebook_likes") == 0){
 		movie->actor_1_facebook_likes = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "actor_1_facebook_likes", atoi(value)); }
+
 	}
-	else if (strcmp(columnName, "gross")){
+	else if (strcmp(columnName, "gross") == 0){
 		movie->gross = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "gross", atoi(value)); }
 	}
-	else if (strcmp(columnName, "genres")){
+	else if (strcmp(columnName, "genres") == 0){
 		movie->genres = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->genres, value);
+		if (DEBUG) { printf("%s %s\n", "genres", value); }
 	}
-	else if (strcmp(columnName, "actor_1_name")){
+	else if (strcmp(columnName, "actor_1_name") == 0){
 		movie->actor_1_name = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->actor_1_name, value);
+		if (DEBUG) { printf("%s %s\n", "actor_1_name", value); }
 	}
-	else if (strcmp(columnName, "movie_title")){
+	else if (strcmp(columnName, "movie_title") == 0){
 		movie->movie_title = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->movie_title, value);
+		if (DEBUG) { printf("%s %s\n", "movie_title", value); }
 	}
-	else if (strcmp(columnName, "num_voted_users")){
+	else if (strcmp(columnName, "num_voted_users") == 0){
 		movie->num_voted_users = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "num_voted_users", atoi(value)); }
 	}
-	else if (strcmp(columnName, "cast_total_facebook_likes")){
+	else if (strcmp(columnName, "cast_total_facebook_likes") == 0){
 		movie->cast_total_facebook_likes = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "cast_total_facebook_likes", atoi(value)); }
 	}
-	else if (strcmp(columnName, "actor_3_name")){
+	else if (strcmp(columnName, "actor_3_name") == 0){
 		movie->actor_3_name = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->actor_3_name, value);
+		if (DEBUG) { printf("%s %s\n", "actor_3_name", value); }
 	}
-	else if (strcmp(columnName, "facenumber_in_poster")){
+	else if (strcmp(columnName, "facenumber_in_poster") == 0){
 		movie->facenumber_in_poster = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "facenumber_in_poster", atoi(value)); }
 	}
-	else if (strcmp(columnName, "plot_keywords")){
+	else if (strcmp(columnName, "plot_keywords") == 0){
 		movie->plot_keywords = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->plot_keywords, value);
+		if (DEBUG) { printf("%s %s\n", "plot_keywords", value); }
 	}
-	else if (strcmp(columnName, "movie_imdb_link")){
-		movie->plot_keywords = malloc(sizeof(char) * strlen(value));
-		strcpy(movie->plot_keywords, value);
+	else if (strcmp(columnName, "movie_imdb_link") == 0){
+		movie->movie_imdb_link = malloc(sizeof(char) * strlen(value));
+		strcpy(movie->movie_imdb_link, value);
+		if (DEBUG) { printf("%s %s\n", "movie_imdb_link", value); }
 	}
-	else if (strcmp(columnName, "num_user_for_reviews")){
+	else if (strcmp(columnName, "num_user_for_reviews") == 0){
 		movie->num_user_for_reviews = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "num_user_for_reviews", atoi(value)); }
 	}
-	else if (strcmp(columnName, "language")){
+	else if (strcmp(columnName, "language") == 0){
 		movie->language = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->language, value);
+		if (DEBUG) { printf("%s %s\n", "language", value); }
 	}
-	else if (strcmp(columnName, "country")){
+	else if (strcmp(columnName, "country") == 0){
 		movie->country = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->country, value);
+		if (DEBUG) { printf("%s %s\n", "country", value); }
 	}
-	else if (strcmp(columnName, "content_rating")){
+	else if (strcmp(columnName, "content_rating") == 0){
 		movie->content_rating = malloc(sizeof(char) * strlen(value));
 		strcpy(movie->content_rating, value);
+		if (DEBUG) { printf("%s %s\n", "content_rating", value); }
 	}
-	else if (strcmp(columnName, "budget")){
+	else if (strcmp(columnName, "budget") == 0){
 		movie->budget = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "budget", atoi(value)); }
 	}
-	else if (strcmp(columnName, "title_year")){
+	else if (strcmp(columnName, "title_year") == 0){
 		movie->title_year = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "title_year", atoi(value)); }
 	}
-	else if (strcmp(columnName, "actor_2_facebook_likes")){
+	else if (strcmp(columnName, "actor_2_facebook_likes") == 0){
 		movie->actor_2_facebook_likes = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "actor_2_facebook_likes", atoi(value)); }
 	}
-	else if (strcmp(columnName, "imdb_score")){
+	else if (strcmp(columnName, "imdb_score") == 0){
 		movie->imdb_score = atof(value);
+		if (DEBUG) { printf("%s %f\n", "imdb_score", atof(value)); }
 	}
-	else if (strcmp(columnName, "aspect_ratio")){
+	else if (strcmp(columnName, "aspect_ratio") == 0){
 		movie->aspect_ratio = atof(value);
+		if (DEBUG) { printf("%s %f\n", "aspect_ratio", atof(value)); }
 	}
-	else if (strcmp(columnName, "movie_facebook_likes")){
+	else if (strcmp(columnName, "movie_facebook_likes") == 0){
 		movie->movie_facebook_likes = atoi(value);
+		if (DEBUG) { printf("%s %d\n", "movie_facebook_likes", atoi(value)); }
 	}
 	return 0;
 }
@@ -237,7 +500,7 @@ int main(int argc, char **argv){
 	}
 
 	// print out column names for testing
-	if(DEBUG){ for(i = 0; i < numColumns; i++){ printf("column: %s\n", columnNames[i]); } }
+	if(DEBUG){ for(i = 0; i < numColumns; i++){ printf("column: %s at number %d\n", columnNames[i], i); } }
 
 	// now want to read in a row and put appropriate data in fields for the row object
 	// to read in a row, we start at new line or EOF
@@ -248,16 +511,21 @@ int main(int argc, char **argv){
 	char currCellText[100];
 	int currCellTextIndex = 0;
 	int cellNumber = 0;	
-	if(DEBUG){ printf("Line %d\n", __LINE__);}
+	if(DEBUG2){ printf("Line %d\n", __LINE__);}
 	movieLine* currMovie = malloc(sizeof(movieLine));
 	initMovieLine(currMovie);
 	movieLineLL* moviesLL = malloc(sizeof(movieLineLL));
 	moviesLL->head = currMovie;
 	moviesLL->rear = currMovie;
 	moviesLL->size = 1;
-	if(DEBUG){ printf("Line %d\n", __LINE__);}
+	bool quotationMark = false;
+	if(DEBUG2){ printf("Line %d\n", __LINE__);}
 	while(read(0, &buffer, 1) != 0){
-		if (buffer == ',')
+		if(buffer == '"'){
+			quotationMark = !quotationMark;
+			continue;
+		}
+		if (buffer == ',' && !quotationMark)
 		{
 			// fill out the struct field based on this info
 			currCellText[currCellTextIndex] = '\0';
@@ -292,7 +560,7 @@ int main(int argc, char **argv){
 	currCellText[currCellTextIndex] = '\0';
 	addFieldToMovie(cellNumber, columnNames, currMovie, currCellText);
 
-
+	//printMoviesAsCsv(moviesLL, numColumns, columnNames);
 
 
 	return 0;
